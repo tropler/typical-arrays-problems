8080bc864771386ccce37b17decf7b46c6d9b4c9
Kanstantsin Sharakouski <texet1711@gmail.com>
2021-02-14 21:02:20 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 
 exports.min = function min (array) {
-  return 0;
+    return Array.isArray(array) && array.length !== 0 ? Math.min(...array) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    return Array.isArray(array) && array.length !== 0 ? Math.max(...array) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    return Array.isArray(array) && array.length !== 0 ?
+        array.reduce((result, current) => result + current / array.length, 0).toFixed(3) : 0;
 }
